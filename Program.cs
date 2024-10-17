c374fb4837bce35700ef0063d1399dc5380b3ce3
squeakysquak <69624825+squeakysquak@users.noreply.github.com>
2024-10-17 14:22:27 +0800

    setup basic CLI program and add gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+namespace microsoft.botsay;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter an integer (first operand)");
+        string ?firstOperand = Console.ReadLine();
+        Console.WriteLine("First Operand: " + firstOperand);
+    }
+
+    private bool IsInteger(string input)
+    {
+        return false;
+    }
+}
