eff2d0ed950c26b0981d8a3f70bb40de340d0802
squeakysquak <69624825+squeakysquak@users.noreply.github.com>
2024-10-17 14:27:19 +0800

    implement integer check for operands

--- Program.cs
+++ Program.cs
@@ -7,12 +7,28 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter an integer (first operand)");
-        string ?firstOperand = Console.ReadLine();
+
+        bool firstOperandCheck = false;
+        string? firstOperand = "";
+
+        while (!firstOperandCheck)
+        {
+            firstOperand = Console.ReadLine();
+            if (IsInteger(firstOperand))
+            {
+                firstOperandCheck = true;
+            }
+            else
+            {
+                Console.WriteLine("Error: Please enter an integer (first operand)");
+            }
+        }
+
         Console.WriteLine("First Operand: " + firstOperand);
     }
 
-    private bool IsInteger(string input)
+    private static bool IsInteger(string input)
     {
-        return false;
+        return int.TryParse(input, out int n);
     }
 }
