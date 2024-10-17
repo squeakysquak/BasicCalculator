b9aa13e233ec3a0695b0677557530b2f46f36bd5
squeakysquak <69624825+squeakysquak@users.noreply.github.com>
2024-10-17 14:40:34 +0800

    add operator base class and plus operator child class

---
+++ Operator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    internal abstract class Operator
+    {
+        public static Operator CreateOperatorFromString(string input)
+        {
+            if (input == "+")
+            {
+                return new PlusOperator();
+            }
+            else
+            {
+                throw new ArgumentException("Error: Invalid operator");
+            }
+        }
+
+        public abstract int DoOperation(int firstOperand, int secondOperand);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using BasicCalculator;
+using System.Reflection;
 
 namespace microsoft.botsay;
 
@@ -9,12 +10,12 @@ internal class Program
         Console.WriteLine("Enter an integer (first operand)");
 
         bool firstOperandCheck = false;
-        string? firstOperand = "";
+        string? firstOperandString = "";
 
         while (!firstOperandCheck)
         {
-            firstOperand = Console.ReadLine();
-            if (IsInteger(firstOperand))
+            firstOperandString = Console.ReadLine();
+            if (IsInteger(firstOperandString))
             {
                 firstOperandCheck = true;
             }
@@ -24,7 +25,29 @@ internal class Program
             }
         }
 
+        int firstOperand = int.Parse(firstOperandString);
         Console.WriteLine("First Operand: " + firstOperand);
+
+        Console.WriteLine("Enter an operator (+, -, / or *)");
+
+        bool operatorCheck = false;
+        string? operatorString = "";
+        Operator op;
+
+        while (!operatorCheck)
+        {
+            operatorString = Console.ReadLine();
+            try
+            {
+                op = Operator.CreateOperatorFromString(operatorString);
+                operatorCheck = true;
+            }
+            catch
+            {
+                Console.WriteLine("Error: Please enter a valid operator (+, -, / or *)");
+            }
+        }
+
     }
 
     private static bool IsInteger(string input)
